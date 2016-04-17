b66d14f99c762213b7b773297c8a13f0a30c15e2
alwaysbollu <always.bollu@gmail.com>
2016-04-17 11:44:11 -0500

    Changed the api key

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -1,4 +1,4 @@
-Rails.application.config.google_key = 'AIzaSyCDMwWQt3tkC_QR4k_F7sOIVaaxp8T0-yw'
+Rails.application.config.google_key = 'AIzaSyDyKaPV4Oq0e5wpcKwyqV-hKolT1Bqc5vs'
 Rails.application.config.zomato_key = '11011d809a56cac69e055aed8212f53f'
 Rails.application.config.alchemy_key = 'f6b4cd87b655593769ab72270aa2af0e727edfc0'
 Rails.application.config.text_razor_key = '8138a302a19f3c249342a0a25fd1cdcae3c6cf373ec6b642766e3f7c'
